1965ff812a4cb593dc8914896ea3a90c27d481f3
akalitenya <akalitenya@mprove.io>
2023-01-02 04:20:10 +0500

    release

--- Formula/mprove.rb
+++ Formula/mprove.rb
@@ -1,9 +1,9 @@
 class Mprove < Formula
   desc "Mprove CLI"
   homepage "https://github.com/mprove-io/mprove"
-  url "https://github.com/mprove-io/mprove-files/releases/download/v-8.2.0/mprove-cli-macos-v-8.2.0.tgz"
-  sha256 "c2bf17a1090f6aaf9c07cf69cf04b2ee346c5bccadee177d64d4c405c59b4227"
-  version "8.2.0"
+  url "https://github.com/mprove-io/mprove-files/releases/download/v-8.2.1/mprove-cli-macos-v-8.2.1.tgz"
+  sha256 "7b262051e6e1e9bb1590db81fc1a7a7277e8582dd3163f3972f69993dcd944f7"
+  version "8.2.1"
   def install
     bin.install "mprove"
   end
